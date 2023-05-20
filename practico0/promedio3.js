f72409ee3e75fc69e96866ef8cc4b9ebafe290d6
HugoMReal <realhmaximiliano@gmail.com>
2023-05-20 12:42:25 -0300

    Practico No Finalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,20 @@
+// calcular promedio 1
+let a = 5;
+let b = 9;
+let c = 9;
+let promedio = ((a+b+c)/3);
+console.log (promedio); 
+
+// calcular promedio 2
+a = -2;
+b = -3;
+c = -8;
+promedio = ((a+b+c)/3);
+console.log (promedio); 
+
+// calcular promedio 3
+a = -1.2;
+b = 5.3;
+c = -3.3;
+promedio = ((a+b+c)/3);
+console.log (promedio); 
