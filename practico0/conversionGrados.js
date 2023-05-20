cebff853e6c3e7cac381e61bc77c40d5883d36c0
HugoMReal <realhmaximiliano@gmail.com>
2023-05-20 12:35:48 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,9 @@
+// inciso a
+let gradosf = 50;
+let conversionC = (gradosf-32)*(5/9);
+console.log(conversionC);
+
+// inciso b 
+gradosf = 75
+conversionC = (gradosf-32)*(5/9);
+console.log(conversionC);
