4d107a80c8009d742ffecdac5cbc27cf113460d3
HugoMReal <realhmaximiliano@gmail.com>
2023-06-07 18:52:39 -0300

    Ejericio 3

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,19 @@
+let var1 = 3;
+
+let var2 = 1;
+
+let var3 = 4;
+
+let var4 = 15;
+
+let suma = var1 + var2;
+
+let resta = var4 - var3;
+
+let resultadofinal = suma * resta;
+
+let esPar = resultadofinal % 2;
+
+console.log("Mis variables iniciales fueron:",var1,",",var2,",",var3," y ",var4, ". La respuesta a verificar si el resultado final es par es:", esPar===0)
+
+
