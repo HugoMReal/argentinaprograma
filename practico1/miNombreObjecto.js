adbb46c9d0fb5352606347adbd8ea31e3cff4d36
HugoMReal <realhmaximiliano@gmail.com>
2023-06-06 21:22:36 -0300

    Ejercicio 2

---
+++ practico1/miNombreObjecto.js
@@ -0,0 +1,8 @@
+// Defino objeto persona
+
+let persona = {nombre:"Hugo Maximiliano", apellido: "Real", edad: "32 años"}
+
+//Con JSON.stringify devuelve todos los valores del objeto
+
+console.log( "Mi objeto persona es el siguiente: " + JSON.stringify (persona));
+
