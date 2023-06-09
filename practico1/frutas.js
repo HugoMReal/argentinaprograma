997fc719715cead6f9fbc5d9cdeebca057636de5
HugoMReal <realhmaximiliano@gmail.com>
2023-06-09 20:44:28 -0300

    Ejercicio 5

---
+++ practico1/frutas.js
@@ -0,0 +1,13 @@
+let frutas = [];
+
+frutas = ["durazno","damasco","manzana","pera","pelón"];
+
+//al colocar los corchetes, se busca el elemento en la posicion que detalla el contenido de estos.
+
+ultima = frutas[frutas.length-1];
+
+console.log("La ultima fruta agregada fue:",ultima);
+
+primera = frutas[0];
+
+console.log("La primera fruta agregada fue:",primera);
